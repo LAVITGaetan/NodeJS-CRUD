8ee32350911911f7c67e4e87fa00f689083b27bc
Gaetan LAVIT <g.lavit@outlook.fr>
2022-04-07 09:14:03 +0400

    env

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,6 +2,19 @@ var Userdb = require('../model/model');
 
 // create and save a user
 exports.create = (req,res)=>{
+    // validate request
+    if(!req.body) {
+        res.status(400).send({message:'Body cannot be empty'});
+        return;
+    }
+
+//new user
+const user = new Userdb({
+    name : req.body.name,
+    email : req.body.email,
+    gender : req.body.gender,
+    status: req.body.status
+})
 
 }
 
