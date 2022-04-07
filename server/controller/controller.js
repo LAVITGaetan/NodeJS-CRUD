37823e3436928380249347583c1893c6e9ab1b5f
Gaetan LAVIT <g.lavit@outlook.fr>
2022-04-07 09:31:57 +0400

    Ajout du controller Create et première insertion avec postman

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -8,13 +8,23 @@ exports.create = (req,res)=>{
         return;
     }
 
-//new user
+// retrieve new user data
 const user = new Userdb({
     name : req.body.name,
     email : req.body.email,
     gender : req.body.gender,
     status: req.body.status
 })
+
+// save user in database
+user
+  .save(user)
+  .then(data => {
+      res.send(data)
+  })
+  .catch(err => {
+      res.status(500).send({message : err.message || "SOme error occured while creating the user"});
+  });
 
 }
 
