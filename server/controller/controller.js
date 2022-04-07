e3cdf18606a0395d830f1653a9b091b3824050ee
Gaetan LAVIT <g.lavit@outlook.fr>
2022-04-07 09:11:43 +0400

    Ajout des routes pour l'API

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+// create and save a user
+exports.create = (req,res)=>{
+
+}
+
+// retrieve multiple or single user
+exports.find = (req,res) => {
+
+}
+
+// update selected user with specific id
+exports.update = (req,res) => {
+
+}
+
+// delete selected user with id in request
+exports.delete = (req,res) => {
+
+}
